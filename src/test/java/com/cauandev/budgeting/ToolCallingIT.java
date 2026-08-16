e4130e4fa4d37d7618959fc58cc911e4bae8af1c
Cauan EngDev <reisu.d.cauan@proton.me>
2026-08-16 15:00:29 -0300

    ToolCalling

---
+++ src/test/java/com/cauandev/budgeting/ToolCallingIT.java
@@ -0,0 +1,44 @@
+package com.cauandev.budgeting;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@EnabledIfEnvironmentVariable(named = "GEMINI_API_KEY", matches = ".+")
+public class ToolCallingIT {
+    @Autowired
+    OpenAiChatModel openAiChatModel;
+
+    static class MathTools {
+        @Tool(description = "Soma dois números inteiros, a e b")
+        public int sum(int a, int b) {
+            return a + b;
+        }
+
+        @Tool(description = "Subtrai dois números inteiros, a e b")
+        public int diff(int a, int b) {
+            return a - b;
+        }
+    }
+
+    @Test
+    void should_executeSum_when_prompted() {
+        var chatClient = ChatClient.builder(openAiChatModel)
+                .defaultSystem("Você é um matemático")
+                .defaultTools(new MathTools())
+                .build();
+
+        var response = chatClient.prompt("Some 10 mais 20. Depois subtraia 30 do resultado anterior. Exiba apenas o resultado final sem explicação!")
+                .call().content();
+
+        assertThat(response).contains("0");
+        System.out.println(response);
+    }
+}
