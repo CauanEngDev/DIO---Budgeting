3532c07608039217d4a77a15f7648efca8861434
Cauan EngDev <reisu.d.cauan@proton.me>
2026-08-15 21:38:08 -0300

    Setup do Projeto e Integração das LLMs

---
+++ src/test/java/com/cauandev/budgeting/BudgetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauandev.budgeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
