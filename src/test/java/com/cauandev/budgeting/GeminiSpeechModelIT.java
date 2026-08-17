49662eebe20abad310bc6314014ffba717e8a118
Cauan EngDev <reisu.d.cauan@proton.me>
2026-08-16 23:51:29 -0300

    Speech API

---
+++ src/test/java/com/cauandev/budgeting/GeminiSpeechModelIT.java
@@ -0,0 +1,35 @@
+package com.cauandev.budgeting;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@EnabledIfEnvironmentVariable(named = "GEMINI_API_KEY", matches = ".+")
+public class GeminiSpeechModelIT {
+    @Autowired
+    OpenAiAudioSpeechModel openAiSpeechModel;
+
+    @Test
+    public void should_produceAudio_when_textIsProvided() throws IOException {
+        var response = openAiSpeechModel.call("O valor total deste serviço ficou 80 reais. Posso confirmar o pagamento?");
+
+        assertThat(response).hasSizeGreaterThan(1024);
+
+        var tempFile = Files.createTempFile("AUDIO_", ".mp3");
+        Files.write(tempFile, response);
+
+        System.out.println(tempFile.toAbsolutePath());
+    }
+}
